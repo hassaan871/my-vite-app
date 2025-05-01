599bda5e5153891c776d0b2615a2671660c510e1
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-05-01 18:32:11 +0500

    Destructured props and state

---
+++ src/Greet.jsx
@@ -0,0 +1,13 @@
+const Greet = (props) => {
+    const { name, heroName} = props;
+
+    return(
+        <div style={{ display: "flex", justifyContent: "center",  }}>
+            <h1>
+                Hello {name} a.k.a {heroName}
+            </h1>
+        </div>
+    )
+}
+
+export default Greet;
